8cb932c313ef56d35876a3a6caf677082508055a
Kanat Kuanyshbek <kunkka163@gmail.com>
2019-09-13 15:47:27 +0600

    Initial Commit

---
+++ testNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testNews
+//
+//  Created by Kanat Kuanyshbek on 9/13/19.
+//  Copyright © 2019 Kanat Kuanyshbek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
